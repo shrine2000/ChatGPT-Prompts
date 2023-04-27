a2c652dc5468e238415c458e0c9545f4afb6d9e7
Shrine Sabu <shrine02sabu@gmail.com>
2023-04-27 23:23:38 +0530

    changed UI from button to list

--- content.js
+++ content.js
@@ -1,35 +1,43 @@
 const form = document.querySelector('form');
 const input = form.querySelector('textarea');
 
-// create buttons
-const button1 = createButton('Convert this code in Java - ', '80px');
-const button2 = createButton('Fix this code - ', '140px');
-const button3 = createButton('Convert this passage into points', '200px');
-const button4 = createButton('Please fix the grammar in my text - ', '260px');
+// create list container
+const listContainer = document.createElement('ul');
+listContainer.style.position = 'fixed';
+listContainer.style.bottom = '80px'; // set the initial position of the container
+listContainer.style.right = '20px';
+listContainer.style.height = '200px'; // set the height of the container
+listContainer.style.overflow = 'scroll'; // enable scrolling
 
-// add buttons to the page
-document.body.appendChild(button1);
-document.body.appendChild(button2);
-document.body.appendChild(button3);
-document.body.appendChild(button4);
+// create list items
+const listItem1 = createListItem('Convert this code in Java - ');
+const listItem2 = createListItem('Fix this code - ');
+const listItem3 = createListItem('Convert this passage into points - ');
+const listItem4 = createListItem('Please fix the grammar in my text - ');
 
-// add click event listener to all buttons
-function handleButtonClick(event) {
+// add list items to the container
+listContainer.appendChild(listItem1);
+listContainer.appendChild(listItem2);
+listContainer.appendChild(listItem3);
+listContainer.appendChild(listItem4);
+
+// add list container to the page
+document.body.appendChild(listContainer);
+
+// add click event listener to all list items
+function handleListItemClick(event) {
   input.value = event.target.textContent;
 }
 
-button1.addEventListener('click', handleButtonClick);
-button2.addEventListener('click', handleButtonClick);
-button3.addEventListener('click', handleButtonClick);
-button4.addEventListener('click', handleButtonClick);
+listItem1.addEventListener('click', handleListItemClick);
+listItem2.addEventListener('click', handleListItemClick);
+listItem3.addEventListener('click', handleListItemClick);
+listItem4.addEventListener('click', handleListItemClick);
 
-// helper function to create button element
-function createButton(text, bottom) {
-  const button = document.createElement('button');
-  button.textContent = text;
-  button.style.position = 'fixed';
-  button.style.bottom = bottom;
-  button.style.right = '20px';
-  button.classList.add('button-34');
-  return button;
+// helper function to create list item element
+function createListItem(text) {
+  const listItem = document.createElement('li');
+  listItem.textContent = text;
+  listItem.classList.add('list-item');
+  return listItem;
 }
