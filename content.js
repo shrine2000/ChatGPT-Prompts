f94dfe432319c6fb77f25785f3dd9dcb762b7155
Shrine Sabu <shrine02sabu@gmail.com>
2023-05-04 16:41:37 +0530

    bug fix

--- content.js
+++ content.js
@@ -1,20 +1,14 @@
-const form = document.querySelector('form');
-const input = form.querySelector('textarea');
-
 const fab = document.createElement('button');
 fab.classList.add('fab');
 fab.textContent = '+';
 fab.style.position = 'fixed';
 fab.style.bottom = '50px';
 fab.style.right = '20px';
-
 fab.classList.add('floating-action-button');
 fab.addEventListener('click', handleFabClick);
-
 document.body.appendChild(fab);
 
 function handleFabClick() {
-  // create list container
   const listContainer = document.createElement('ul');
   listContainer.style.position = 'fixed';
   listContainer.style.bottom = '80px';
@@ -22,30 +16,39 @@ function handleFabClick() {
   listContainer.style.height = '200px';
   listContainer.style.overflow = 'scroll';
 
-  // create list items
-  const listItem1 = createListItem('convert this code into Java - ');
+  const listItem1 = createListItem('convert this code into java - ');
   const listItem2 = createListItem('fix this code - ');
   const listItem3 = createListItem('convert this passage into points - ');
   const listItem4 = createListItem('fix the grammar in this text - ');
 
-  // add list items to the container
   listContainer.appendChild(listItem1);
   listContainer.appendChild(listItem2);
   listContainer.appendChild(listItem3);
   listContainer.appendChild(listItem4);
-
-  // add click event listener to all list items
-  function handleListItemClick(event) {
-    input.value = event.target.textContent;
-    listContainer.remove(); // remove the list container after an item is clicked
-  }
 
   listItem1.addEventListener('click', handleListItemClick);
   listItem2.addEventListener('click', handleListItemClick);
   listItem3.addEventListener('click', handleListItemClick);
   listItem4.addEventListener('click', handleListItemClick);
 
-  // helper function to create list item element
+  document.body.appendChild(listContainer);
+
+  function handleListItemClick(event) {
+    const form = document.querySelector('form');
+    const input = form?.querySelector('textarea');
+
+    if (input == null) {
+      const textarea = document.querySelector('textarea');
+      const dataId = textarea.getAttribute('data-id');
+      const inputarea = document.querySelector(`[data-id="${dataId}"]`);
+      inputarea.value = event.target.textContent;
+    } else {
+      input.value = event.target.textContent;
+    }
+
+    listContainer.remove();
+  }
+
   function createListItem(text) {
     const listItem = document.createElement('li');
     listItem.textContent = text;
@@ -53,15 +56,11 @@ function handleFabClick() {
     return listItem;
   }
 
-  // add click event listener to the document body
   document.body.addEventListener('click', handleDocumentClick);
 
   function handleDocumentClick(event) {
     if (!listContainer.contains(event.target) && event.target !== fab) {
-      listContainer.remove(); // remove the list container if the user clicks outside of it
+      listContainer.remove();
     }
   }
-
-  document.body.appendChild(listContainer);
 }
-
